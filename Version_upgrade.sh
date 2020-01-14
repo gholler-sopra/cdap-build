9ef1c13dc51e5265641cf492b3eb82c0e17d2e69
ashish-madaan-guavus <ashish.madaan@guavus.com>
2020-01-14 13:52:58 +0530

    [RAFD-3635]: Updated Version Upgrade file

--- Version_upgrade.sh
+++ Version_upgrade.sh
@@ -1,10 +1,10 @@
-git submodule foreach --recursive git clean -xfd && \
-		git reset --hard  && \
-		git submodule foreach --recursive git reset --hard && \
-		git submodule update --remote && \
-		git submodule update --init --recursive --remote
+#git submodule foreach --recursive git clean -xfd && \
+#		git reset --hard  && \
+#		git submodule foreach --recursive git reset --hard && \
+#		git submodule update --remote && \
+#		git submodule update --init --recursive --remote
 
-#!/bin/bash
+
 ## declare an array variable
 declare -a arrayT=()
 for item in `git config --file .gitmodules --get-regexp submodule  | awk '{ print $2 }'`
@@ -14,6 +14,8 @@ done;
 
 # get length of an array
 arraylength=${#arrayT[@]}
+home_dir=$(pwd)
+#echo "$home_dir";
 
 # use for loop to read all values and indexes
 for (( i=0; i<${arraylength}; i=i+3 ));
@@ -26,13 +28,10 @@ do
   find . -name "pom.xml-e" -type f -delete;
   find . -name "*.iml" -type f -delete;
   git checkout $branch;
-  git checkout -b dev/RAFD-3635
-  git add --all;
+  git add --A;
   git commit -m 'version Changed';
-  git push origin dev/RAFD-3635;
-#  git branch;
-  pwd
-  cd ..;
+  git push origin $branch;
+  cd $home_dir;
 #  echo  " cd $directory; git checkout $branch; echo changeSomething; git add -A; git commit -m 'version changed'; git push ; cd ..; ";
 
 done
