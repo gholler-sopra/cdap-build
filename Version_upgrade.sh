a29b8aeafc81d05dd834303d8d872487ce5efce4
ashish-madaan-guavus <ashish.madaan@guavus.com>
2020-01-15 11:27:06 +0530

    [RAFD-3635]: Updated Version Upgrade file

--- Version_upgrade.sh
+++ Version_upgrade.sh
@@ -1,8 +1,8 @@
-#git submodule foreach --recursive git clean -xfd && \
-#		git reset --hard  && \
-#		git submodule foreach --recursive git reset --hard && \
-#		git submodule update --remote && \
-#		git submodule update --init --recursive --remote
+git submodule foreach --recursive git clean -xfd && \
+git reset --hard  && \
+git submodule foreach --recursive git reset --hard && \
+git submodule update --remote && \
+git submodule update --init --recursive --remote
 
 ## declare an array variable
 declare -a arrayT=()
@@ -14,11 +14,6 @@ done;
 # get length of an array
 arraylength=${#arrayT[@]}
 home_dir=$(pwd)
-#echo "$home_dir";
-
-read -p "Enter Old Version: "  old_version
-read -p "Enter New Version: "  new_version
-#echo $new_version;
 
 # use for loop to read all values and indexes
 for (( i=0; i<${arraylength}; i=i+3 ));
@@ -28,7 +23,7 @@ do
   url="${arrayT[$i+1]}";
   cd $directory;
   git checkout $branch;
-  grep -lr --include=pom.xml "$old_version" * | xargs sed -i -e 's/$old_version/$new_version/g';
+  grep -lr --include=pom.xml "5.1.217" * | xargs sed -i -e 's/5.1.217/5.1.216/g';
   find . -name "pom.xml-e" -type f -delete;
   find . -name "*.iml" -type f -delete;
   git add -A;
