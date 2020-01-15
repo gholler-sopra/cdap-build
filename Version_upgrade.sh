6ef86129ec9961bcdfe6fe04a3307aad6d7bd007
ashish-madaan-guavus <ashish.madaan@guavus.com>
2020-01-15 11:48:44 +0530

    [RAFD-3635]: Updated Version Upgrade file

--- Version_upgrade.sh
+++ Version_upgrade.sh
@@ -14,7 +14,12 @@ done;
 # get length of an array
 arraylength=${#arrayT[@]}
 home_dir=$(pwd)
+#echo "$home_dir";
 
+read -p "Enter Old Version: "  old_version
+read -p "Enter New Version: "  new_version
+echo $new_version;
+echo $old_version;
 # use for loop to read all values and indexes
 for (( i=0; i<${arraylength}; i=i+3 ));
 do
@@ -23,7 +28,7 @@ do
   url="${arrayT[$i+1]}";
   cd $directory;
   git checkout $branch;
-  grep -lr --include=pom.xml "5.1.216" * | xargs sed -i -e 's/5.1.216/5.1.217/g';
+  grep -lr --include=pom.xml "$old_version" * | xargs sed -i -e 's/"$old_version"/"$new_version"/g';
   find . -name "pom.xml-e" -type f -delete;
   find . -name "*.iml" -type f -delete;
   git add -A;
